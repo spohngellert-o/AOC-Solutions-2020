edd5d70884a47b30f091c37475e7a7d9c29dbe90
spohngellert-o <spohngellert.o@northeastern.edu>
2020-12-15 14:44:56 -0500

    Cleaned up to use queues.

--- 15-1/src/main/scala/Main.scala
+++ 15-1/src/main/scala/Main.scala
@@ -1,3 +1,6 @@
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
 object Main {
 
   def main(args: Array[String]): Unit = {
@@ -5,15 +8,19 @@ object Main {
     println(getNthNum(nums, 2020))
   }
   def getNthNum(nums: List[Long], end: Int): Long = {
-    val spoken_map = nums.zipWithIndex.map(v => (v._1, List(v._2 + 1))).toMap
+    val spoken_map = nums.zipWithIndex.map(v => (v._1, Queue(v._2 + 1))).toMap
     val cur_turn = nums.length + 1
     val last_spoken = nums.last
-    def run(cm: Map[Long, List[Int]], turn: Int, ls: Long): Long = {
+    @tailrec
+    def run(cm: Map[Long, Queue[Int]], turn: Int, ls: Long): Long = {
+      if (turn % 1000000 == 0) println(turn)
       if (turn == end + 1) ls
-      else if (cm(ls).length == 1) run(cm.updated(0.toLong, cm.getOrElse(0.toLong, List()).appended(turn)), turn+1, 0.toLong)
+      else if (cm(ls).length == 1) run(cm.updated(0.toLong, cm.getOrElse(0.toLong, Queue()).appended(turn)), turn+1, 0.toLong)
       else {
-        val next_v = (turn - 1) - cm(ls).takeRight(2).head
-        run(cm.updated(next_v, cm.getOrElse(next_v, List()).appended(turn)), turn+1, next_v)
+        val (ppv, q) = cm(ls).dequeue
+        val cmu = cm.updated(ls, q)
+        val next_v = (turn - 1) - ppv
+        run(cmu.updated(next_v, cmu.getOrElse(next_v, Queue()) :+ (turn)), turn+1, next_v)
       }
     }
     run(spoken_map, cur_turn, last_spoken)
--- 15-2/src/main/scala/Main.scala
+++ 15-2/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import scala.annotation.tailrec
+import scala.collection.immutable.Queue
 
 object Main {
 
@@ -7,16 +8,19 @@ object Main {
     println(getNthNum(nums, 30000000))
   }
   def getNthNum(nums: List[Long], end: Int): Long = {
-    val spoken_map = nums.zipWithIndex.map(v => (v._1, (v._2 + 1, v._2 + 1))).toMap
+    val spoken_map = nums.zipWithIndex.map(v => (v._1, Queue(v._2 + 1))).toMap
     val cur_turn = nums.length + 1
     val last_spoken = nums.last
     @tailrec
-    def run(cm: Map[Long, (Int, Int)], turn: Int, ls: Long): Long = {
+    def run(cm: Map[Long, Queue[Int]], turn: Int, ls: Long): Long = {
       if (turn % 1000000 == 0) println(turn)
       if (turn == end + 1) ls
+      else if (cm(ls).length == 1) run(cm.updated(0.toLong, cm.getOrElse(0.toLong, Queue()).appended(turn)), turn+1, 0.toLong)
       else {
-        val next_v = (turn - 1) - cm(ls)._1
-        run(cm.updated(next_v, (cm.getOrElse(next_v, (turn, turn))._2, turn)), turn+1, next_v)
+        val (ppv, q) = cm(ls).dequeue
+        val cmu = cm.updated(ls, q)
+        val next_v = (turn - 1) - ppv
+        run(cmu.updated(next_v, cmu.getOrElse(next_v, Queue()) :+ (turn)), turn+1, next_v)
       }
     }
     run(spoken_map, cur_turn, last_spoken)
